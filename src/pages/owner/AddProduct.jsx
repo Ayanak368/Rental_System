d9bd327807d4e843e68196bf898c75e4d720a392
Ayanak03 <ayanakbhaskar03@gmail.com>
2025-10-02 19:58:46 +0530

    update dashboard

--- src/pages/owner/AddProduct.jsx
+++ src/pages/owner/AddProduct.jsx
@@ -1,13 +1,137 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Title from '../../components/owner/Title'
+import { assets } from '../../assets/assets'
 
 const AddProduct = () => {
 
+  const currency=import.meta.env.VITE_CURRENCY
 
+  const [image, setImage] = useState(null)
+  const [product, setProduct] = useState({
+    category: '',
+    name: '',
+    size: '',
+    pricePerDay: '',
+    color: '',
+    occasion: '',
+    material: '',
+    description: '',
+    location:'',
+
+  })
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault()
+  }
 
     
   return (
-    <div>
-      
+    <div className='px-4 py-10 md:px-10 flex-1'>
+      <Title title="Add New Product" subtitle="Fill in details to list a new item for booking, including pricing,availability, and car specification" />
+      <form onSubmit={onSubmitHandler} className=' flex flex-col gap-5 text-gray-500 text-sm mt-6 max-w-xl'>
+
+        {/* car image*/}
+        <div className='flex items-center gap-2 w-full'>
+          <label htmlFor="product-image">
+            <img src={image ? URL.createObjectURL(image) : assets.upload_icon} alt="" className='h-14 rounded cursor-pointer'/>
+            <input type="file" id="product-image" accept='image/*' hidden onChange={e =>
+              setImage(e.target.files[0])}/>
+          </label>
+          <p className='text-sm text-gray-500'>Upload the picture of the item</p>
+        </div>
+        { /* name and category*/}
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
+          <div className='flex flex-col w-full'>
+            <label>Name</label>
+            <input type="text" placeholder='e.g. MidNight blue lehanga....' required
+              className='px-3 py-2 mt-1 border border-borderColor rounded-md outline-none'
+              value={product.name}  onChange={e=> setProduct({...product,name : e.target.value})}/>
+          </div>
+          <div className='flex flex-col w-full'>
+            <label>Material</label>
+            <input type="text" placeholder='e.g.silk,cotton....' required
+              className='px-3 py-2 mt-1 border border-borderColor rounded-md outline-none'
+              value={product.material}  onChange={e=> setProduct({...product,material : e.target.value})}/>
+          </div>
+        </div>
+
+        { /*price,size,category*/}
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6'>
+        <div className='flex flex-col w-full'>
+            <label>Size</label>
+            <input type="text" placeholder='e.g.M,XL,XXL' required
+              className='px-3 py-2 mt-1 border border-borderColor rounded-md outline-none'
+              value={product.size}  onChange={e=> setProduct({...product,size : e.target.value})}/>
+          </div>
+        <div className='flex flex-col w-full'>
+            <label>Daily Price ({currency})</label>
+            <input type="number" placeholder='0' required
+              className='px-3 py-2 mt-1 border border-borderColor rounded-md outline-none'
+              value={product.pricePerDay}  onChange={e=> setProduct({...product,pricePerDay : e.target.value})}/>
+          </div>
+        <div className='flex flex-col w-full'>
+            <label>Category</label>
+            <select onChange={e => setProduct({ ...product, category: e.target.value })}
+              value={product.category} className='px-3 py-2 mt-1 border border-borderColor rounded-md
+            outline-none'>
+              <option value="">Select a Category</option>
+              <option value="Lehanga">Lehanga</option>
+              <option value="Gown">Gown</option>
+              <option value="Necklace">Necklace</option>
+              <option value="Earrings">Earrings</option>
+            </select>
+          </div>
+        </div>
+
+        { /*occasion,color*/}
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6'>
+        <div className='flex flex-col w-full'>
+            <label>Color</label>
+            <input type="text" placeholder='e.g.red,green.....' required
+              className='px-3 py-2 mt-1 border border-borderColor rounded-md outline-none'
+              value={product.color}  onChange={e=> setProduct({...product,color : e.target.value})}/>
+          </div>
+        <div className='flex flex-col w-full'>
+            <label>Occasion</label>
+            <input type="text" placeholder='e.g.wedding....' required
+              className='px-3 py-2 mt-1 border border-borderColor rounded-md outline-none'
+              value={product.occasion}  onChange={e=> setProduct({...product,occasion : e.target.value})}/>
+          </div>
+        </div>
+        { /*Location*/}
+        <div className='flex flex-col w-full'>
+        <label>Location</label>
+            <select onChange={e => setProduct({ ...product, location: e.target.value })}
+              value={product.location} className='px-3 py-2 mt-1 border border-borderColor rounded-md
+            outline-none'>
+              <option value="">Select a Location</option>
+              <option value="Calicut">Calicut</option>
+              <option value="kannur">kannur</option>
+              <option value="Trivandrum">Trivandrum</option>
+              <option value="Kollam">Kollam</option>
+            </select>
+        </div>
+        {/*Description*/}
+        <div className='flex flex-col w-full'>
+            <label>Description</label>
+            <textarea rows={5} type="text" placeholder='Flowing silk gown with sequin details. Ideal for evening parties and receptions.' required
+              className='px-3 py-2 mt-1 border border-borderColor rounded-md outline-none'
+            value={product.description} onChange={e => setProduct({ ...product, description: e.target.value })}>
+            </textarea>
+          </div>
+        <div className='flex flex-col w-full'>
+            <label>Full Description</label>
+            <textarea rows={5} type="text" placeholder='Midnight Blue Gown Lehenga is a stunning silk gown perfect for .......' required
+              className='px-3 py-2 mt-1 border border-borderColor rounded-md outline-none'
+            value={product.fullDescription} onChange={e => setProduct({ ...product,fullDescription: e.target.value })}>
+            </textarea>
+          </div>
+        <button className='flex items-center gap-2 px-4 py-2.5 mt-4 bg-primary text-white rounded-md font-medium 
+         w-max cursor-pointer'>
+          <img src={assets.tick_icon} alt="" />
+          List Your Colloctions
+         </button>
+      </form>
     </div>
   )
 }
--- src/pages/owner/ManageProduct.jsx
+++ src/pages/owner/ManageProduct.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { assets, dummyCarData } from '../../assets/assets'
+import Title from '../../components/owner/Title'
 
 const ManageProduct = () => {
+
+  const currency=import.meta.env.VITE_CURRENCY
+
+  const [product, setProduct] = useState([])
+  
+  const fetchOwnerProducts = async () => {
+    setProduct(dummyCarData)
+  }
+
+  useEffect(() => {
+    fetchOwnerProducts()
+  },[])
   return (
-    <div>
+    <div className='px-4 pt-10 md:px-10 w-full'>
+      <Title title='Manage Products' subtitle='View all listed items,update their detail,or remove them from the booking platform' />
       
+      <div className='max-w-3xl w-full rounded-md overflow-hidden border border-borderColor mt-6'>
+        <table className='w-full border-collapse text-left text-sm text-gray-600'>
+          <thead className='text-gray-500'>
+            <tr>
+              <th className='p-3 font-medium'>Items</th>
+              <th className='p-3 font-medium max-md:hidden'>Category</th>
+              <th className='p-3 font-medium'>Price</th>
+              <th className='p-3 font-medium max-md:hidden'>Status</th>
+              <th className='p-3 font-medium'>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {product.map((product, index) => (
+              <tr key={index} className='border-t border-borderColor'>
+                <td className='p-3 flex items-center gap-3'>
+                  <img src={product.image} alt="" className='h-12 aspect-square rounded-md object-cover'/>
+                  <div className='max-md:hidden'>
+                    <p className='font-medium'>{product.name} </p>
+                    <p className='text-xs text-gray-500'>{product.size}</p>
+                    <p className='text-xs text-gray-500'>{product.color}</p>
+                 </div>
+                </td>
+                <td className='p-3 max-md:hidden'>{product.category}</td>
+                <td className='p-3 max-md:hidden'>{currency}{product.pricePerDay}/day </td>
+                <td className='p-3 max-md:hidden'>
+                  <span className={`px-3 py-1 rounded-full text-xs ${product.isAvailable ? 'bg-green-100 text-green-500'
+                    : 'bg-red-100 text-red-500'}`}>
+                    {product.isAvailable?"Available":"Unavailable"}
+               </span>
+                </td>
+                <td className='flex items-center p-3'>
+                  <img src={product.isAvailable ? assets.eye_close_icon : assets.eye_icon} alt="" className='cursor-pointer' />
+                  <img src={ assets.delete_icon} alt="" className='cursor-pointer' />
+
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
